4783dc74b4b947f5d87cbe4964c227b2faf989d0
N3tw0rk3r <world-admin@hotmail.com>
2018-11-16 23:54:20 +0300

    Update TwoNumCal.java

--- TwoNumCal.java
+++ TwoNumCal.java
@@ -78,15 +78,12 @@ public class Main {
     public static double add (double num1 , double num2) {
         return num1 + num2;
     }
-
     public static double sub (double num1 , double num2) {
         return num1 - num2;
     }
-
     public static double mult (double num1 , double num2) {
         return num1 * num2;
     }
-
     public static double div (double num1 , double num2) {
         return num1 / num2;
     }
